1ee2b3837890aff5621d5eff419593fdf605ecc6
unknown <dalidasy24@gmail.com>
2021-02-23 23:13:38 +0100

    first comasamit

--- QuizProject/README.md
+++
@@ -1,2 +0,0 @@
-# QuizProject
-# QuizProject
--- QuizProject/game.js
+++ QuizProject/game.js
@@ -1,58 +1,90 @@
-const question = document.getElementById("question")
+const question = document.getElementById("question");
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 
-
-let availableQuesions = [];
 let currentQuestion = {};
 let acceptingAnswers = false;
-let questionCounter = 0;
 let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
 
+let questions = [
+  {
+    question: "Inside which HTML element do we put the JavaScript??",
+    choice1: "<script>",
+    choice2: "<javascript>",
+    choice3: "<js>",
+    choice4: "<scripting>",
+    answer: 1
+  },
+  {
+    question:
+      "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    choice1: "<script href='xxx.js'>",
+    choice2: "<script name='xxx.js'>",
+    choice3: "<script src='xxx.js'>",
+    choice4: "<script file='xxx.js'>",
+    answer: 3
+  },
+  {
+    question: " How do you write 'Hello World' in an alert box?",
+    choice1: "msgBox('Hello World');",
+    choice2: "alertBox('Hello World');",
+    choice3: "msg('Hello World');",
+    choice4: "alert('Hello World');",
+    answer: 4
+  }
+];
+
+//CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 10;
+const MAX_QUESTIONS = 3;
 
-startgame = () => {
-    score = 0;
-    questionCounter = 0;
-    availableQuesions = [...questions];
-    getNewQuestion();
-}
+startGame = () => {
+  questionCounter = 0;
+  score = 0;
+  availableQuesions = [...questions];
+  getNewQuestion();
+};
 
 getNewQuestion = () => {
-    if (questionCounter >= MAX_QUESTIONS || availableQuesions.length === 0) {
-        return window.location.assign("./end.html")
-    }
+  if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+    //go to the end page
+    return window.location.assign("/end.html");
+  }
+  questionCounter++;
+// bu kisim soruyu ekrana yazdiriyor
+  const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+  currentQuestion = availableQuesions[questionIndex];
+  question.innerText = currentQuestion.question;
 
-    questionCounter++;
-    const questionIndex = Math.floor(Math.random() * availableQuesions.length)
-    currentQuestion = availableQuesions[questionIndex];
-    question.innerHTML = currentQuestion.question;
-    choices.forEach(choice => {
-        const number = choice.dataset["number"];
-        choice.innerHTML = currentQuestion["choice" + number];
-    })
-    availableQuesions.splice(questionIndex, 1);
-    acceptingAnswers = true;
+  
+  choices.forEach(choice => {
+    const number = choice.dataset["number"];
+    choice.innerText = currentQuestion["choice" + number];
+  });
 
-
-}
+  availableQuesions.splice(questionIndex, 1);
+  acceptingAnswers = true;
+};
 
 choices.forEach(choice => {
-    choice.addEventListener("click", e => {
-        if (!acceptingAnswers)
-            return;
-        const selectedChoice = e.target;
-        const selectedAnswer = selectedChoice.dataset["number"]
-        const classToApply = selectedAnswer == currentQuestion.answer ? "correct" : "incorrect"
-        selectedChoice.parentElement.classList.add(classToApply);
+  choice.addEventListener("click", e => {
+    if (!acceptingAnswers) return;
 
-        setTimeout(() => {
-            selectedChoice.parentElement.classList.remove(classToApply);
-            getNewQuestion();
-        }, 3000);
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset["number"];
 
-    })
-})
+    const classToApply =
+      selectedAnswer == currentQuestion.answer ? "correct" : "incorrect";
 
-startgame();
+    selectedChoice.parentElement.classList.add(classToApply);
 
+    setTimeout(() => {
+      selectedChoice.parentElement.classList.remove(classToApply);
+      getNewQuestion();
+    }, 1000);
+  });
+});
+
+startGame();
